542c877968170fa5fb8418d0f058bd8e38b72fd8
Chaaivisva <126050738+Chaaivisva@users.noreply.github.com>
2024-05-23 21:37:51 +0530

    comments added

--- orphan process.c
+++ orphan process.c
@@ -1,37 +1,28 @@
-#include<stdio.h>                                                                                                                                                      #include <stdlib.h> 
-
-#include <pthread.h> 
-
+#include<stdio.h> 
+#include <sys/types.h> 
 #include <unistd.h> 
-
-void worker(void *);  
-
-int main(int argc, char **argv){ 
-
-         pthread_t t1;  
-
-         int thread_id = 1; 
-
-        if ( (pthread_create(&t1, NULL, (void *)&worker, (void *)&thread_id)) != 0) { 
-
-               printf("Error creating thread\n"); 
-
-               exit(1); 
-
-        }    
-
-        pthread_join(t1, NULL); 
-
-        return 0; 
-
+  
+int main() 
+{ 
+    // Create a child process       
+    int pid = fork(); 
+  
+    if (pid > 0) 
+        printf("in parent process"); 
+  
+    // Note that pid is 0 in child process 
+    // and negative if fork() fails 
+    else if (pid == 0) 
+    { 
+        sleep(30); 
+        printf("in child process"); 
+    } 
+  
+    return 0; 
 } 
 
-void worker(void *a)  { 
-
-         int *cnt = (int *)a; 
-
-          printf("This is thread %d\n", *cnt); 
-
-          pthread_exit(0); 
-
-} 
+// .a.out &  ==> process number 
+// pstree -p process number 
+// ps aux | grep a.out ==> another process number
+// ps -o ppid= another process number
+// pstree -p 1
